eb29133bba1c653f3e769e18d10bce098a93673e
Chi Pham <chipt.318@gmail.com>
2018-03-08 01:24:54 +0700

    initial code

--- Session02/Homework/9x9.py
+++ Session02/Homework/9x9.py
@@ -1,10 +1,16 @@
 for i in range(1, 10):
     for j in range(1, 10):
-        print(int(i*j), end= " ")
+        if int(i*j) < 10:
+            print("",int(i*j), end= " ")
+        else:
+            print(int(i*j), end= " ")
     print()
 
 n = int(input("choose a number:"))
 for a in range(1, n+1):
     for b in range(1, n+1):
-        print(int(a*b), end= " ")
+        if int(a*b) < 10:
+            print("",int(a*b), end= " ")
+        else:
+            print(int(a*b), end= " ")
     print()
---
+++ Session02/Homework/xephinh.py
@@ -0,0 +1,6 @@
+# n = int(input("Choose your size:"))
+# for i in range(1, n+1):
+#
+
+for i in range(3):
+    print("*"*3, " "*1, "*"*2, " "*3, "*"*3)
