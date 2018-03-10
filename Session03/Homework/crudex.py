043c1e5100766862e1a807d57351b3498589fc26
Chi Pham <chipt.318@gmail.com>
2018-03-11 00:51:56 +0700

    initial commit

--- Session03/Homework/crudex.py
+++ Session03/Homework/crudex.py
@@ -1,12 +1,18 @@
 store = ["t-shirt", "sweater"]
-print(*store, sep=", ")
-n = input("What else?")
-store.append(n)
-print(*store, sep=", ")
-pos = int(input("Update position:"))
-new = input("New item:")
-store[pos-1] = new
-print(*store, sep=", ")
-pos1 = int(input("Delete position: "))
-del store[pos1-1]
-print(*store, sep=", ")
+n = input("What do you want?")
+if n == "R":
+    print(*store, sep=", ")
+elif n == "C":
+    new = input("What else")
+    store.append(new)
+    print(*store, sep=", ")
+elif n == "U":
+    pos = int(input("Update position:"))
+    new = input("New item:")
+    store[pos-1] = new
+    print(*store, sep=", ")
+
+elif n == "D":
+    pos1 = int(input("Delete position: "))
+    del store[pos1-1]
+    print(*store, sep=", ")
